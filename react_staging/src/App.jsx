dbe42e130abe166e8815a4964e7e0c9c290eab5f
陈俊沪 <junhuchen@126.com>
2023-07-03 13:55:49 +0800

    0000

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,24 +1,10 @@
 import React, { Component } from 'react'
-import { Button,DatePicker } from 'antd';
-import {WechatOutlined,WeiboOutlined,SearchOutlined} from '@ant-design/icons'
-const { RangePicker } = DatePicker;
-
+import Todolist from './components/ToDoList';
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				App....
-				<button>点我</button>
-				<Button type="primary">按钮1</Button>
-				<Button >按钮2</Button>
-				<Button type="link">按钮3</Button>
-				<Button type="primary" icon={<SearchOutlined />}>
-					Search
-				</Button>
-				<WechatOutlined />
-				<WeiboOutlined />
-				<DatePicker/>
-				<RangePicker/>
+				<Todolist />
 			</div>
 		)
 	}
