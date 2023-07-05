a6c78a44ab14c4be5f951b53afb8fecf7afaceb0
陈俊沪 <junhuchen@126.com>
2023-07-05 14:37:46 +0800

    077(路由基础)

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,45 +1,38 @@
 import React, { Component } from 'react'
-import './App.css'
+import {Link,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+
 export default class App extends Component {
 	render() {
 		return (
-			<div className="container">
-				<section className="jumbotron">
-					<h3 className="jumbotron-heading">Search Github Users</h3>
-					<div>
-						<input type="text" placeholder="enter the name you search" />&nbsp;<button>Search</button>
-					</div>
-				</section>
+			<div>
 				<div className="row">
-					<div className="card">
-						<a href="https://github.com/reactjs" target="_blank">
-							<img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-						</a>
-						<p className="card-text">reactjs</p>
+					<div className="col-xs-offset-2 col-xs-8">
+						<div className="page-header"><h2>React Router Demo</h2></div>
 					</div>
-					<div className="card">
-						<a href="https://github.com/reactjs" target="_blank">
-							<img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-						</a>
-						<p className="card-text">reactjs</p>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+
+							{/* 原生html中，靠<a>跳转不同的页面 */}
+							{/* <a className="list-group-item" href="./about.html">About</a>
+							<a className="list-group-item active" href="./home.html">Home</a> */}
+
+							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
+							<Link className="list-group-item" to="/about">About</Link>
+							<Link className="list-group-item" to="/home">Home</Link>
+						</div>
 					</div>
-					<div className="card">
-						<a href="https://github.com/reactjs" target="_blank">
-							<img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-						</a>
-						<p className="card-text">reactjs</p>
-					</div>
-					<div className="card">
-						<a href="https://github.com/reactjs" target="_blank">
-							<img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-						</a>
-						<p className="card-text">reactjs</p>
-					</div>
-					<div className="card">
-						<a href="https://github.com/reactjs" target="_blank">
-							<img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-						</a>
-						<p className="card-text">reactjs</p>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								{/* 注册路由 */}
+								<Route path="/about" component={About}/>
+								<Route path="/home" component={Home}/>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
