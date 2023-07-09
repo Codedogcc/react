08691244d04a12f87b9edf82b17ce34ceeadd1bf
陈俊沪 <junhuchen@126.com>
2023-07-09 15:46:18 +0800

    redux精简版

--- redux_test/1_src_纯react版自己写的/App.jsx
+++ redux_test/1_src_纯react版自己写的/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import Count from './components/Count'
+import Count1 from './components/Count1'
+// import Count1 from './components/Count'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Count/>
+				<Count1 />
 			</div>
 		)
 	}
--- redux_test/1_src_纯react版自己写的/components/Count/index.jsx
+++ redux_test/1_src_纯react版自己写的/components/Count/index.jsx
@@ -1,48 +1,42 @@
 import React, { Component } from 'react'
-//引入store，用于获取redux中保存状态
-import store from '../../redux/store'
 
 export default class Count extends Component {
 
-	state = {carName:'奔驰c63'}
-
-	/* componentDidMount(){
-		//检测redux中状态的变化，只要变化，就调用render
-		store.subscribe(()=>{
-			this.setState({})
-		})
-	} */
+	state = {count:0}
 
 	//加法
 	increment = ()=>{
 		const {value} = this.selectNumber
-		store.dispatch({type:'increment',data:value*1})
+		const {count} = this.state
+		this.setState({count:count+value*1})
 	}
 	//减法
 	decrement = ()=>{
 		const {value} = this.selectNumber
-		store.dispatch({type:'decrement',data:value*1})
+		const {count} = this.state
+		this.setState({count:count-value*1})
 	}
 	//奇数再加
 	incrementIfOdd = ()=>{
 		const {value} = this.selectNumber
-		const count = store.getState()
+		const {count} = this.state
 		if(count % 2 !== 0){
-			store.dispatch({type:'increment',data:value*1})
+			this.setState({count:count+value*1})
 		}
 	}
 	//异步加
 	incrementAsync = ()=>{
 		const {value} = this.selectNumber
+		const {count} = this.state
 		setTimeout(()=>{
-			store.dispatch({type:'increment',data:value*1})
+			this.setState({count:count+value*1})
 		},500)
 	}
 
 	render() {
 		return (
 			<div>
-				<h1>当前求和为：{store.getState()}</h1>
+				<h1>当前求和为：{this.state.count}</h1>
 				<select ref={c => this.selectNumber = c}>
 					<option value="1">1</option>
 					<option value="2">2</option>
