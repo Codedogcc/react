acf6ed2e4861c4e56459d489e13a108b5042538e
陈俊沪 <junhuchen@126.com>
2023-07-09 15:17:36 +0800

    求和案例000

--- redux_test/src/App.jsx
+++ redux_test/src/App.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import Count from './containers/Count' //引入的Count的容器组件
-import Person from './containers/Person' //引入的Person的容器组件
+import Count1 from './components/Count1'
+// import Count1 from './components/Count'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<Count/>
-				<hr/>
-				<Person/>
+				<Count1 />
 			</div>
 		)
 	}
