2dc6f4abbf21bca7d729aa9b821f4f676738ac12
陈俊沪 <junhuchen@126.com>
2023-07-10 15:07:42 +0800

    104

--- redux_test/4_src_异步action版/redux/store.js
+++ redux_test/4_src_异步action版/redux/store.js
@@ -3,10 +3,11 @@
 */
 
 //引入createStore，专门用于创建redux中最为核心的store对象
-import {createStore,applyMiddleware} from 'redux'
+//引入applyMiddleware，专门用于支持中间件，让store识别func
+import { createStore, applyMiddleware } from 'redux'
 //引入为Count组件服务的reducer
 import countReducer from './count_reducer'
 //引入redux-thunk，用于支持异步action
 import thunk from 'redux-thunk'
 //暴露store
-export default createStore(countReducer,applyMiddleware(thunk))
+export default createStore(countReducer, applyMiddleware(thunk))
