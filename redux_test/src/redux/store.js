9bb8f5a9c29388f8f3f369f158fda9f26dfcdf18
陈俊沪 <junhuchen@126.com>
2023-07-11 17:39:05 +0800

    113

--- redux_test/src/redux/store.js
+++ redux_test/src/redux/store.js
@@ -8,5 +8,6 @@ import {createStore,applyMiddleware} from 'redux'
 import countReducer from './count_reducer'
 //引入redux-thunk，用于支持异步action
 import thunk from 'redux-thunk'
+
 //暴露store
 export default createStore(countReducer,applyMiddleware(thunk))
